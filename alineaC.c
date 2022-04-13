4dc9cbec5d8ff4effe27e59acb02018429229473
Pedro Meneses <a17551@alunos.ipca.pt>
2022-04-13 19:30:07 +0100

    Correções e documentação de código

--- alineaC.c
+++ alineaC.c
@@ -3,40 +3,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-
-// Feito por Pedro Meneses
+ /**
+   * @brief Acrescenta origem destino
+   * Este comando deve acrescentar(append) de tudo que estiver presente no ficheiro.txt no ficheiroAppend.txt
+   *  Caso algum dos ficheiros não exista, deve ser apresentado um aviso ao utilizador.
+   * Autor: Pedro Meneses
+   */
 
 int main(){
+    int fd, fd2;
+    char c;
 
-    /**
-     * Esta alinea deverá criar um ficheiro com o nome passado como parametro na função open e escrever no ficheiro
-     * Se algum ficheiro nao existir , imprimir erro
-     */
-
-    int fd = open("./ficheiro.txt", O_RDONLY); // Designar o nosso fd
-    if (fd == -1) {  // Caso dê erro
+    fd = open("./ficheiro.txt", O_RDONLY); // Designar o nosso fd
+    if (fd == -1) {  // Caso dê erro ao abrir ficheiro, devolver erro ao utilizador
         perror("erro, o ficheiro não existe ou o caminho é inválido");
         exit(1);
     }
     else{
-        // Imprime todos os caracteres do ficheiro
-        char c;
-        int fd2 = open("./ficheiroAppend.txt", O_APPEND | O_WRONLY | O_CREAT, 0644);
+        fd2 = open("./ficheiroAppend.txt", O_APPEND | O_WRONLY); // Abrir o ficheiro, caso exista fazemos append e escrevemos no ficheiro de destino
 
         if(fd2 == -1){
-             perror("erro, o ficheiro.copia não existe ou o caminho é inválido");
-            exit(1);
+             perror("erro, o ficheiro que queremos fazer append não existe ou o caminho é inválido");
+            exit(1); // Caso dê erro.
         }
         else{
 
             while (read(fd, &c, 1) > 0) {
-                write(fd2, &c, 1);
+                write(fd2, &c, 1); // Escreve no ficheiro
             }
-
         }
     }
-
+    close(fd);
+    close(fd2);
 
     return 0;
 }
