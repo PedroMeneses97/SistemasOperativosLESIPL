86fca81054ff4e017a3f3012cd678ef471e0033f
rdmf17 <rdmf17rdmf@gmail.com>
2022-04-19 22:35:59 +0100

    Changes

--- alineaD.c
+++ alineaD.c
@@ -3,14 +3,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 /**
- * @brief Feito por Ricardo Fernandes
+ * @brief conta ficheiro- Este comando deve apresentar no ecrã o
+ * número de linhas no ficheiro indicado.
+ * Caso o ficheiro nao exista, o comando deve avisar que o ficheiro não existe.
+ * Feito por Ricardo Fernandes
  */
-int main(){
+int main()
+{
 
     int fd = open("./ficheiro2.txt", O_RDONLY); // Designar o nosso fd
-  
+
     if (fd == -1)
     { // Caso dê erro
         perror("erro, o ficheiro não existe ou o caminho é inválido");
@@ -18,21 +21,26 @@ int main(){
     }
     else
     {
-        // Imprime todos os caracteres do ficheiro
         char c;
         int count = 0;
 
-        while (read(fd, &c, 1) > 0 )
+        // Corre todos os caracters do ficheiro e por cada '\n' ou se
+        // o caracter estive no final do ficheiro adiciona mais um ao valor do contador.
+        while (read(fd, &c, 1) > 0)
         {
 
-            if (c == '\n' || c == EOF){
+            if (c == '\n' || c == EOF)
+            {
                 count++;
             }
-
         }
 
+        // Imprime o valor do contador
         printf("O ficheiro tem %d linhas", count);
+
+        // Fecha o fd
+        close(fd);
     }
 
-    return 0 ;
+    return 0;
 }
